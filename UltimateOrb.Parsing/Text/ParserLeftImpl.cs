0920596b325ac029f47b2ec9af56ca502775921c
YANG-Xiaoyong <876018665@qq.com>
2019-05-26 13:29:02 +0800

    杨小泳 修改LeftParser

--- UltimateOrb.Parsing/Text/ParserLeftImpl.cs
+++ UltimateOrb.Parsing/Text/ParserLeftImpl.cs
@@ -4,19 +4,37 @@ using UltimateOrb.Parsing.Text;
 
 namespace UltimateOrb.Parsing {
 
-    public readonly struct ParserLeftImpl<T1, T2>
-        : IParser<T1> {
+    public readonly struct ParserLeftImpl
+        : IParser<string> {
 
-        private readonly IParser<char, T1> parser1;
-        private readonly IParser<char, T2> parser2;
+        private readonly string left;
+        private readonly string right;
 
-        public ParserLeftImpl(IParser<char, T1> parser1, IParser<char, T2> parser2) {
-            this.parser1 = parser1;
-            this.parser2 = parser2;
+        public ParserLeftImpl(string left, string right) {
+            this.left = left;
+            this.right = right;
         }
 
-        public IEnumerator<(T1 Result, int Position)> Parse<TString>(TString input, int position = 0) where TString : IReadOnlyList<char> {
-            return parser1.Parse(input, position);
+        public IEnumerator<(string Result, int Position)> Parse<TString>(TString input, int position = 0) where TString : IReadOnlyList<char> {
+            var p = position;
+            if (p + left.Length + right.Length <= input.Count) {
+                for (var i = 0; left.Length > i;) {
+                    var ch1 = left[i++];
+                    var ch2 = input[p++];
+                    if (ch1 != ch2) {
+                        yield break;
+                    }
+                }
+                var pleft = p;
+                for (var i = 0; right.Length > i;) {
+                    var ch1 = right[i++];
+                    var ch2 = input[p++];
+                    if (ch1 != ch2) {
+                        yield break;
+                    }
+                }
+                yield return (this.left, pleft);
+            }
         }
     }
 }
