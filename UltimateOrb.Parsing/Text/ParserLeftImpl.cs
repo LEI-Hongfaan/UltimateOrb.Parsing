39e0ae1bb8d35521378ce78143acb48c360e63f8
YANG-Xiaoyong <876018665@qq.com>
2019-06-01 22:51:50 +0800

    杨小泳 修改left

--- UltimateOrb.Parsing/Text/ParserLeftImpl.cs
+++ UltimateOrb.Parsing/Text/ParserLeftImpl.cs
@@ -17,30 +17,13 @@ namespace UltimateOrb.Parsing {
 
         public IEnumerator<(T1 Result, int Position)> Parse<TString>(TString input, int position = 0) where TString : IReadOnlyList<char> {
             var parser1_enumerator = parser1.Parse(input, position);
-            var resultList = new List<(T1 Result, int Position)>();
-            var result = false;
-            var right_start_position = 0;
             for (; parser1_enumerator.MoveNext();) {
-                right_start_position = parser1_enumerator.Current.Position;
-                resultList.Add(parser1_enumerator.Current);
-            }
-            if (right_start_position > 0) {
+                var right_start_position = parser1_enumerator.Current.Position;
                 var parser2_enumerator = parser2.Parse(input, right_start_position);
                 for (; parser2_enumerator.MoveNext();) {
-                    result = true;
-                    break;
+                    yield return (parser1_enumerator.Current.Result, parser2_enumerator.Current.Position);
                 }
                 parser2_enumerator.Dispose();
-            }
-            
-            if (result) {
-                foreach (var t in resultList) {
-                    yield return (t.Result, t.Position);
-                }
-                /*parser1_enumerator.Reset();
-                for (; parser1_enumerator.MoveNext();) {
-                    yield return (parser1_enumerator.Current.Result, parser1_enumerator.Current.Position);
-                }*/
             }
             parser1_enumerator.Dispose();
         }
